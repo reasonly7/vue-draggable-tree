b40a7fe4070726784a80f8513cd8671a4f72279a
leishanglin <reasonly8@sina.com>
2024-12-18 18:28:53 +0800

    feat: add actions

--- src/composables/useOperateAction.ts
+++ src/composables/useOperateAction.ts
@@ -6,10 +6,16 @@ export type Action =
   | "add-inner"
   | "remove"
   | "rename"
-  | "move-top"
+  | "edit"
+  | "move-up"
   | "move-down"
+  | "move-top"
+  | "move-bottom"
   | "cut"
-  | "paste";
+  | "paste"
+  | "undo"
+  | "redo"
+  | "save";
 
 export const useOperateAction = () => {
   const actions = computed(() => {
@@ -17,7 +23,17 @@ export const useOperateAction = () => {
       { key: "add", icon: "add", disabled: true },
       { key: "add-inner", icon: "diff-added", disabled: true },
       { key: "remove", icon: "trash", disabled: true },
-      { key: "rename", icon: "edit", disabled: true },
+      { key: "rename", icon: "symbol-parameter", disabled: true },
+      { key: "edit", icon: "edit", disabled: true },
+      { key: "move-up", icon: "arrow-up", disabled: true },
+      { key: "move-down", icon: "arrow-down", disabled: true },
+      { key: "move-top", icon: "fold-up", disabled: true },
+      { key: "move-bottom", icon: "fold-down", disabled: true },
+      { key: "cut", icon: "remote", disabled: true },
+      { key: "paste", icon: "copy", disabled: true },
+      { key: "undo", icon: "discard", disabled: true },
+      { key: "redo", icon: "redo", disabled: true },
+      { key: "save", icon: "save", disabled: true },
     ];
     return list;
   });
